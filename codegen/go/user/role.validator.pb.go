6b57cb00d47cdadb4a09658b4a26948a7efe8713
surendrakandel <20468112+surendrakandel@users.noreply.github.com>
2021-09-30 14:26:15 -0700

    few minor changes

--- codegen/go/user/role.validator.pb.go
+++ codegen/go/user/role.validator.pb.go
@@ -5,12 +5,12 @@ package user
 
 import (
 	fmt "fmt"
-	math "math"
 	proto "github.com/golang/protobuf/proto"
+	github_com_mwitkow_go_proto_validators "github.com/mwitkow/go-proto-validators"
+	_ "github.com/surendrakandel/proto/codegen/go/opts"
 	_ "google.golang.org/genproto/googleapis/api/annotations"
 	_ "google.golang.org/protobuf/types/known/emptypb"
-	_ "github.com/surendrakandel/proto/codegen/go/opts"
-	github_com_mwitkow_go_proto_validators "github.com/mwitkow/go-proto-validators"
+	math "math"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
--- go.mod
+++ go.mod
@@ -9,7 +9,7 @@ require (
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.6.0
 	github.com/mwitkow/go-proto-validators v0.3.2
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
-	golang.org/x/net v0.0.0-20210520170846-37e1c6afe023 // indirect
+	golang.org/x/net v0.0.0-20210929193557-e81a3d93ecf6 // indirect
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
 	google.golang.org/genproto v0.0.0-20210903162649-d08c68adba83
 	google.golang.org/grpc v1.40.0
